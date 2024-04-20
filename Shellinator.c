df58af3b2012fd16ee8643493f4ab9dea931caf3
Magnusmajo <alexisoblivion@gmail.com>
2024-04-20 22:05:47 +0000

    Fix

--- Shellinator.c
+++ Shellinator.c
@@ -1,44 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "shell.h"
 
 #define MAX_INPUT_LENGTH 100
 
-int main() {
-    char input[MAX_INPUT_LENGTH];
-    char* command;
+int main()
+{
+	char input[MAX_INPUT_LENGTH];
+	char* command = "$ ";
 
-    while (1) {
-        printf("#cisfun$ "); // Print the shell prompt
+	while (1)
+	{
+		printf("#shelinator %s", command); // Print the shell prompt
 
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            // Handle Ctrl+D (EOF)
-            printf("\nExiting shell.\n");
-            break;
-        }
+	if (fgets(input, sizeof(input), stdin) == NULL) {
+            // Handle Ctrl+D (End of file detected)
+		printf("\nBye Bye\n");
+		break;
+	}
 
         // Remove newline character from input
-        input[strcspn(input, "\n")] = '\0';
+	input[strlen(input) - 1] = '\0';
 
-        // Execute the command
-        command = input;
-        pid_t childPid = fork();
+	 // Check if the command is"exit" to end the shell
+	if (strcmp(input, "exit") == 0)
+	{
+		printf("Bye Bye...\n");
+		break;
+	}
+	
+	// Execute the command
+	command = input;
+	pid_t childPid = fork();
 
-        if (childPid < 0) {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        } else if (childPid == 0) {
+	if (childPid < 0)
+	{
+		perror("Ups fork failed"); //Print error if fork fail
+		exit(EXIT_FAILURE);
+	}
+	else if (childPid == 0)
+	{
             // Child process
-            execl(command, command, (char*)NULL);
-            perror("execl failed"); // Print error if execl fails
-            exit(EXIT_FAILURE);
-        } else {
-            // Parent process
-            wait(NULL); // Wait for the child to exit
-        }
-    }
+		execl(command, command, (char*)NULL);
+		perror("Ups execl failed"); // Print error if execl fails
+		exit(EXIT_FAILURE);
+	}
+	else
+		{
+		// Parent process
+			wait(NULL); // Wait for the child to exit
+		}
+	}
 
     return 0;
 }
